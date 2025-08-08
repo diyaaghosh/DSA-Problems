ed8912e24d3d50be4e17946694d5d27ca2cb0019
Diya Ghosh <diya1692005ne@gmail.com>
2025-08-08 22:51:36 +0530

    Add files via upload

---
+++ Sorting/insertion_sort.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+void swap(int *a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+void insertion_sort(vector<int>&arr){
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            swap(& arr[j] , & arr[j-1]);
+            j--;
+        }
+    }
+}
+int main(){
+    int n;
+    cout<<"enter the dimension : ";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"enter the elements of array : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"sorted array : ";
+    insertion_sort(arr);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+}
