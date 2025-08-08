99cb7b65272f043742fce55e7d51ac6982659776
Diya Ghosh <diya1692005ne@gmail.com>
2025-08-08 22:14:05 +0530

    Add files via upload

---
+++ Sorting/bubble_sort.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+void swap(int *a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+void bubble_sort(vector<int>&arr){
+    int n=arr.size();
+    for(int i=n-1;i>=1;i--){
+        for(int j=0;j<=i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(&arr[j],&arr[j+1]);
+            }
+        }
+    }
+   
+}
+int main(){
+    int n;
+    cout<<"enter the dimension : ";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"enter the elements of array : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"sorted array : ";
+    bubble_sort(arr);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+}
